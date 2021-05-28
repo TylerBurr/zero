2d9ecef1f553a413268d9352ced2c8cdad6f8009
payton-burr <greatpayton@gmail.com>
2021-05-27 18:20:13 -0600

    feat: queue will send an embedded message containing queued songs

--- commands/index.js
+++ commands/index.js
@@ -1,29 +1,32 @@
-
-const { prefix, help } = require('../utilities/configUtil')
-const execute = require('../controller/execute')
-const stop = require('../controller/stop')
-const pause = require('../controller/pause')
-const skip = require('../controller/skip')
-const resume = require('../controller/resume')
-const viewQueue = require('../controller/viewQueue')
-const nowplaying = require('../controller/nowplaying')
-const search = require('../controller/search')
+const { prefix, help } = require('../utilities/configUtil');
+const execute = require('../controller/execute');
+const stop = require('../controller/stop');
+const pause = require('../controller/pause');
+const skip = require('../controller/skip');
+const resume = require('../controller/resume');
+const viewQueue = require('../controller/viewQueue');
+const nowplaying = require('../controller/nowplaying');
+const search = require('../controller/search');
 
 function start(message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
   if (message.content.startsWith(`${prefix}play`)) {
-      const validate = message.content.includes('https://youtube.com')
-      if (validate == true) {
-        execute.execute(message);
-      } 
-      else if (message.content.includes('https://open.spotify.com/') || message.content.includes('https://soundcloud.com') || message.content.includes('https://music.apple.com' )) {
-        return message.channel.send('We are adding support for Spotify, Soundcloud & Apple Music')
-      } 
-      else {
-        search.search(message)
-        return;
-      }
+    const validate = message.content.includes('https://youtube.com');
+    if (validate == true) {
+      execute.execute(message);
+    } else if (
+      message.content.includes('https://open.spotify.com/') ||
+      message.content.includes('https://soundcloud.com') ||
+      message.content.includes('https://music.apple.com')
+    ) {
+      return message.channel.send(
+        'We are adding support for Spotify, Soundcloud & Apple Music'
+      );
+    } else {
+      search.search(message);
+      return;
+    }
   } else if (message.content.startsWith(`${prefix}skip`)) {
     skip.skip(message);
     return;
@@ -37,18 +40,20 @@ function start(message) {
     resume.resume(message);
     return;
   } else if (message.content.startsWith(`${prefix}queue`)) {
-    viewQueue.viewQueue(message);
-    return;
+    // viewQueue.viewQueue(message);
+    return message.channel.send({ embed: viewQueue.viewQueue(message) });
   } else if (message.content.startsWith(`${prefix}nowplaying`)) {
     nowplaying.nowplaying(message);
     return;
   } else if (message.content.startsWith(`${prefix}help`)) {
     return message.channel.send({ embed: help });
   } else {
-    message.channel.send(`You need to enter a valid command!\n Use ${prefix}help for a list of Commands`);
+    message.channel.send(
+      `You need to enter a valid command!\n Use ${prefix}help for a list of Commands`
+    );
   }
 }
 
 module.exports = {
-    start
-}
+  start,
+};
--- controller/viewQueue.js
+++ controller/viewQueue.js
@@ -1,19 +1,38 @@
 const { queue } = require('../utilities/configUtil');
-function viewQueue(message) {
-    const serverQueue = queue.get(message.guild.id);
-        if (!message.member.voice.channel)
-            return message.channel.send(
-                "You have to be in a voice channel to view the queue!"
-              );
-        if (!serverQueue)
-            return message.channel.send("Unable to queue a song!");
-        serverQueue.songs.forEach(function (value, index) {
-            if (index > 0) {
-                serverQueue.textChannel.send(value.title);
-            }
-        })
-    }
+const Discord = require('discord.js');
 
-  module.exports = {
-    viewQueue
+function viewQueue(message) {
+  const serverQueue = queue.get(message.guild.id);
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      'You have to be in a voice channel to view the queue!'
+    );
+  if (!serverQueue) return message.channel.send('Unable to queue a song!');
+
+  const songQueue = serverQueue.songs.map((song, index) => {
+    return {
+      name: '\u200b',
+      value: `${index + 1} ) ${song.title}`,
+    };
+  });
+
+  return new Discord.MessageEmbed()
+    .setColor('#4a4e69')
+    .setAuthor(
+      'ZERO',
+      'https://i.imgur.com/wSTFkRM.png',
+      'https://discord.js.org'
+    )
+    .setTitle('Song Queue')
+    .addFields(...songQueue)
+    .addField('\u200b', 'End of queue')
+    .addField('\u200b', '\u200b')
+    .setFooter(
+      'https://github.com/TylerBurr/zero',
+      'https://i.imgur.com/wSTFkRM.png'
+    );
 }
+
+module.exports = {
+  viewQueue,
+};
